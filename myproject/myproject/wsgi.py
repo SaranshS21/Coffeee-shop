6185f31af264ebb89b62a9ec472fea2aa07d784a
Saran <saransh.sharma485@gmail.com>
2025-05-11 15:54:56 +0530

    Fix WSGI module path for Render

--- myproject/myproject/wsgi.py
+++ myproject/myproject/wsgi.py
@@ -1,10 +1,6 @@
 import os
-import sys
-
-# Add the parent directory of your Django project to the Python path
-sys.path.append('/opt/render/project/src/myproject')
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
